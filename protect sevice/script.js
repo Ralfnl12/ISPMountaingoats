51683bf39c549640008bc5e41acae7fdb90a2995
Ralfnl12 <78805502+Ralfnl12@users.noreply.github.com>
2023-10-31 14:48:14 +0100

    wdasd

--- protect sevice/script.js
+++ protect sevice/script.js
@@ -6,6 +6,7 @@ document.getElementById("login-form").addEventListener("submit", function (event
 
     // Define your user-password pairs and their associated redirect URLs
     const users = {
+        "wasd": { password: "wasd", redirectUrl: "isp.html" },
         "puppy": { password: "Cute", redirectUrl: "https://bit.ly/473dn8x" },
         "illegal": { password: "Yard", redirectUrl: "https://www.youtube.com/watch?v=h0SNAsocIx8&pp=ygUXaWxsZWdhbCBpbiBteSB5YXJkIHNvbmc%3D" },
         "thegoat": { password: "mountain", redirectUrl: "https://www.youtube.com/watch?v=YjyUIwKPAxA" },
